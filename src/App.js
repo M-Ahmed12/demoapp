8e1bfe5c6e2103dbfb4423438e604a9a876919aa
Ahmed Shahid <Ahmed_shahid50@yahoo.com>
2020-12-03 19:13:14 +0500

    demoapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Dinner from './dinner.js';  
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( <div>
+       <h1>hello world from ahmed</h1>
+
+        <hr/>
+       {/* <Dinner></Dinner> */}
+
+
+           <Dinner dishName="Nihari" sweetdish="kheer"/>
+
+        <hr/> 
+      
+           <Dinner dishName="biryani" sweetdish="jalebi"/>
+
+        <hr/>
+
+           <Dinner dishName="pulao" sweetdish="gajar ka halwa"/>
+        </div>
   );
+
 }
 
 export default App;
+
+var student = {
+  name: 'ahmed',
+  course: 'bootcamp'
+}
